cbed13f34b0e06514394ceb30f85baa45cb8b965
unknown <Ohad.Baehr@gmail.com>
2020-09-27 15:24:30 +0300

    bug fixes with monkey patching?

--- src/Windows/App/Canvas.js
+++ src/Windows/App/Canvas.js
@@ -5,12 +5,17 @@ import './Canvas.less'
 import ReactDOM from 'react-dom';
 import { useSelector } from 'react-redux'
 import globalStore, { saveActiveLayerImage } from '../../Store/StoreFuncs'
+
 var history = {
   redo_list: [],
   undo_list: [],
-  saveState: function (ctx, list) {
+  saveState: function (ctx, list,keep_redo) {
+    keep_redo = keep_redo || false;
+    if (!keep_redo) {
+      this.redo_list = [];
+    }
     let data
-    (list || this.undo_list).push({src:data= ctx.canvas.toDataURL(), id:ctx.canvas.id});
+    (list || this.undo_list).push({ src: data=ctx.canvas.toDataURL(), id: ctx.canvas.id });
     return data
   },
   undo: function (ctx) {
@@ -21,18 +26,19 @@ var history = {
   },
   restoreState: function (ctx, pop, push) {
     if (pop.length) {
-      let index=pop.slice(0).reverse().findIndex(x => x.id === ctx.canvas.id);
-      if(index!==-1){
-        let backwardsIndex=pop.length-1-index
+      let index = pop.slice(0).reverse().findIndex(x => x.id === ctx.canvas.id);
+      if (index !== -1) {
+        let backwardsIndex = pop.length - 1 - index
         this.saveState(ctx, push, true);
-        var restore_state = pop.splice(backwardsIndex,1)[0].src;
+        var restore_state = pop.splice(backwardsIndex, 1)[0].src;
         var img = new Image();
-        img.src = restore_state
         img.onload = function () {
           ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
           ctx.globalCompositeOperation = 'source-over';
           ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+          ctx.img=img
         }
+        img.src = restore_state
         saveActiveLayerImage(restore_state)
       }
     }
@@ -40,20 +46,23 @@ var history = {
 }
 
 const pencil = (ctx, strokeColor) => {
-  let dpi=globalStore.getState().canvas.dpi
+  let dpi = globalStore.getState().canvas.dpi
   ctx.lineWidth = 20;
   ctx.lineJoin = ctx.lineCap = 'round';
   ctx.strokeStyle = "rgba(0,0,0,0.75)"
   let isDrawing, points = [];
-  let img = new Image();
-  function draw(e){
+  function draw(e) {
     let rect = ctx.canvas.getBoundingClientRect();
     let mouseX = e.clientX - rect.left
     let mouseY = e.clientY - rect.top
     let prevCtxOperation = ctx.globalCompositeOperation
     ctx.globalCompositeOperation = 'source-over';
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    try{
+      ctx.drawImage(ctx.img, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    }catch{
+
+    }
     points.push({ x: mouseX, y: mouseY });
     switch (e.buttons) {
       case 1: ctx.globalCompositeOperation = 'source-over';
@@ -65,18 +74,18 @@ const pencil = (ctx, strokeColor) => {
       default:
         break;
     }
-    ctx.scale(dpi,dpi)
+    ctx.scale(dpi, dpi)
     ctx.beginPath();
     if (prevCtxOperation !== ctx.globalCompositeOperation) {
       //fixing issues when switching from one ctx composition to another
       ctx.globalCompositeOperation = prevCtxOperation
       ctx.moveTo(points[0].x, points[0].y);
-      for (var i = 1; i < points.length; i++) {
+      for (var i = 0; i < points.length; i++) {
         ctx.lineTo(points[i].x, points[i].y);
       }
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.stroke();
-      img.src = history.saveState(ctx);
+      ctx.img.src = history.saveState(ctx);
       points.length = 0;
       prevCtxOperation === 'source-over' ? ctx.globalCompositeOperation = 'destination-out' : ctx.globalCompositeOperation = 'source-over'
     } else {
@@ -86,7 +95,7 @@ const pencil = (ctx, strokeColor) => {
       }
       ctx.setTransform(1, 0, 0, 1, 0, 0);
       ctx.stroke();
-    }
+  }
   }
   return {
     onPointerDown: function (e) {
@@ -94,9 +103,9 @@ const pencil = (ctx, strokeColor) => {
         let rect = ctx.canvas.getBoundingClientRect();
         let mouseX = e.clientX - rect.left
         let mouseY = e.clientY - rect.top
-        if(mouseX>=0 && mouseX<=rect.width && mouseY>=0 && mouseY<=rect.height){
+        if (mouseX >= 0 && mouseX <= rect.width && mouseY >= 0 && mouseY <= rect.height) {
           isDrawing = true;
-          img.src = history.saveState(ctx);
+          history.saveState(ctx);
           draw(e)
         }
       }
@@ -106,8 +115,8 @@ const pencil = (ctx, strokeColor) => {
         isDrawing = false;
         points.length = 0;
         ctx.globalCompositeOperation = 'source-over'
-        let data=ctx.canvas.toDataURL()
-        img.src=data
+        let data = ctx.canvas.toDataURL()
+        ctx.img.src = data
         saveActiveLayerImage(data)
       }
     },
@@ -129,7 +138,7 @@ const Canvas = () => {
   const itemsRef = useRef([]);
   let canvasContainer = useRef(null)
   let tool = null
-  
+
   const handleCommands = (e) => {
     console.log("changed")
     if (e.ctrlKey && e.key === 'z') {
@@ -141,20 +150,25 @@ const Canvas = () => {
   }
 
   useEffect(() => {
-    if(store.layers.length){
+    if (store.layers.length) {
       itemsRef.current = itemsRef.current.slice(0, store.layers.length);
       //Here we set up the properties of the canvas element. 
       itemsRef.current.map((el, i) => {
         let ctx = el.getContext('2d')
         let img = new Image()
-        img.src = store.layers[i].src
-        img.onload = function () {
+        img.onload = () => {
           ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-          ctx.drawImage(img, 0, 0, el.width, el.height); 
+          ctx.drawImage(img, 0, 0, el.width, el.height);
+          console.log('LOADED!!!!!!')
+          ctx.img=img
         }
+        img.onerror=()=>{
+          ctx.img=img
+        }
+        img.src = store.layers[i].src
       })
 
-      if(store.layers[store.activeLayer].visible){
+      if (store.layers[store.activeLayer].visible) {
         canvas = itemsRef.current[store.activeLayer]
         tool = pencil(canvas.getContext('2d'), state.strokeColor)
         canvasContainer.current.addEventListener("pointermove", tool.onPointerMove)
@@ -164,28 +178,28 @@ const Canvas = () => {
       window.addEventListener('keydown', handleCommands);
       return () => {
         window.removeEventListener('keydown', handleCommands);
-        if(store.layers[store.activeLayer].visible){
+        if (store.layers[store.activeLayer].visible) {
           window.removeEventListener('pointerup', tool.onPointerUp);
           canvasContainer.current.removeEventListener("pointermove", tool.onPointerMove)
           canvasContainer.current.removeEventListener("pointerdown", tool.onPointerDown)
         }
       }
     }
-  }, [store.activeLayer,store.layersCount,store.dpi])//do not put store.layers as a dependency
+  }, [store.activeLayer, store.layersCount, store.dpi, store.layers[store.activeLayer]?.visible])//do not put store.layers as a dependency
 
   const canvasMap = React.useMemo(() => {
     return store.layers.map((_, index) => {
-      return <canvas id={store.layers[index].id} className={`canvas ${store.layers[index].visible?'':'hidden'}`} key={`canvas-${store.layers[index].id}-key`} ref={el => itemsRef.current[index] = el} 
-       style={{ width: state.canvasWidth, height: state.canvasHeight }} width={state.canvasWidth*store.dpi} height={state.canvasHeight*store.dpi}/>
+      return <canvas id={store.layers[index].id} className={`canvas ${store.layers[index].visible ? '' : 'hidden'}`} key={`canvas-${store.layers[index].id}-key`} ref={el => itemsRef.current[index] = el}
+        style={{ width: state.canvasWidth, height: state.canvasHeight }} width={state.canvasWidth * store.dpi} height={state.canvasHeight * store.dpi} />
     })
-  }, [store.activeLayer,store.layersCount,store.dpi,store.layers])
-  
+  }, [store.activeLayer, store.layersCount, store.dpi, store.layers])
+
   return (
-      <div className={`canvas-container`} style={{ minHeight: state.canvasHeight + 100 }} ref={canvasContainer}>
-        <div className={`transparent-background`} style={{ width: state.canvasWidth, height: state.canvasHeight }}>
-          {canvasMap} 
-        </div>
+    <div className={`canvas-container`} style={{ minHeight: state.canvasHeight + 100 }} ref={canvasContainer}>
+      <div className={`transparent-background`} style={{ width: state.canvasWidth, height: state.canvasHeight }}>
+        {canvasMap}
       </div>
+    </div>
   );
 }
 
